9ca9376eea12fad34016c8c8cbd82009f934242a
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2018-10-06 10:12:20 -0300

    Adding testingBugs examples

---
+++ testingBugs/bugGoBack.js
@@ -0,0 +1,18 @@
+const { Builder, By, Key } = require('selenium-webdriver');
+var driver = new Builder().forBrowser('chrome').build();
+
+async function goBack() {
+    await driver.get('http://ec2-13-59-235-34.us-east-2.compute.amazonaws.com:3000/cadastro');
+    driver.findElement(By.id('btn_voltar')).click().then(function() {
+        return driver.getCurrentUrl();
+    })
+    .then(function(currentUrl) { 
+        if (currentUrl === 'http://ec2-13-59-235-34.us-east-2.compute.amazonaws.com:3000/cadastro') {
+            console.log('BUG: Botão VOLTAR não funciona');
+            console.log('-------------------------------------------------------------------');
+        }
+        return 'Teste passou'
+    });
+} 
+
+goBack();
---
+++ testingBugs/bugLogout.js
@@ -0,0 +1,26 @@
+const { Builder, By, Key } = require('selenium-webdriver');
+var driver = new Builder().forBrowser('chrome').build();
+
+const typeLogin = driver.findElement(By.name('login')).sendKeys('sayuri', Key.TAB);
+const typePassword = driver.findElement(By.name('senha')).sendKeys('123456', Key.RETURN);
+
+async function login() {
+    await driver.get('http://ec2-13-59-235-34.us-east-2.compute.amazonaws.com:3000');
+    Promise.all([typeLogin, typePassword]).catch(reason => {
+        console.log(reason);
+    }).then(() => {
+        driver.findElement(By.id('btn_sair')).click().then(function() {
+            return driver.getCurrentUrl();
+        })
+        .then(function(currentUrl) { 
+            if (currentUrl !== 'http://ec2-13-59-235-34.us-east-2.compute.amazonaws.com:3000/logout') {
+                console.log('BUG: Usuário ainda logado, redirecionando para outra pagina');
+                console.log('Redirect for: '.concat(currentUrl));
+                console.log('-------------------------------------------------------------------');
+            }
+            return 'Teste passou'
+        });
+    })
+}
+
+login()
